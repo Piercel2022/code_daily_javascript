85a59106d322916cd575922f2f88205a21fe5e68
Pierre <piercel.coder2022@gmail.com>
2022-10-29 15:21:38 +0200

    remove solution-1.js and solution-2.js

--- solution-1.js
+++
@@ -1,8 +0,0 @@
-/* How to remove spaces from a string in JS */
-function removeSpaces(orgText){
-    let removedSpace = orgText.split(" ").join("");
-    return removedSpace;
-}
-
-let text = removeSpaces("Dev Tips Daily");
-console.log(text);
--- solution-2.js
+++
@@ -1,5 +0,0 @@
-function area(base, height){
-    let result = (base * height) / 2;
-    return result;
-}
-console.log(area(3, 4))
