d4f02a9d12724b3c7bfa739f480b25565b3231f3
Pierre <piercel.coder2022@gmail.com>
2022-10-29 15:33:33 +0200

    Remove the older file name

--- solution-3.js
+++
@@ -1,7 +0,0 @@
-function isLeapYear(year){
- return year % 400 === 0 || ((year % 4 === 0) && (year % 100 !== 0)) ? true : false;
-        
-}
-
-console.log(isLeapYear(2020));
-console.log(isLeapYear(2022));
