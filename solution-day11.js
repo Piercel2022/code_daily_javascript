2a454c07521174ef57bb001cb946a6db64fd600f
Pierre <piercel.coder2022@gmail.com>
2022-10-30 16:00:38 +0100

    Add the solution for day 11

---
+++ solution-day11.js
@@ -0,0 +1,22 @@
+// Print Hallow Square Star Pattern
+function printPattern(n){
+
+    let string = "";
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < n; j++){
+           if(i === 0 || i === n - 1){
+            string += "*";
+           }else{
+            if (j === 0 || j === n - 1){
+            string += "*";
+            }else{
+            string += " ";
+            }
+           }
+        }
+        string += "\n";
+    }
+    console.log(string);
+}
+
+printPattern(5);
