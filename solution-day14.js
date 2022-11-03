8102bd8562ebcc6c59dcb58e880cf5893871f35a
Pierre <piercel.coder2022@gmail.com>
2022-11-03 11:09:22 +0100

    Add the solution for day 14 - prime numbers

---
+++ solution-day14.js
@@ -0,0 +1,22 @@
+/**
+ * Print prime numbers between given range m to n
+ * @input printPrime(20, 50)
+ * @output 23, 29, 31, 37, 41, 43, 47
+ */
+function checkPrime(num){
+    if(num == 1 || num == 0) return false;
+
+    for(let i = 2; i < num; i++){
+        if(num % i == 0) return false;
+    }
+    return true;
+}
+
+function printPrime(n, m){
+    for(let i = n; i < m; i++){
+        if(checkPrime(i)){
+            console.log(i);
+        }
+    }
+}
+printPrime(20, 50);
